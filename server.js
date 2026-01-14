0142a7b4470161e6a370efe8eef61d2897c9f24e
rakaism <rakadianp@gmail.com>
2026-01-14 11:04:07 +0700

    add: cors package

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const pool = require("./db");
 
+// Middleware
+app.use(cors());
 app.use(express.json());
 
 const eventsRouter = require("./routes/events");
